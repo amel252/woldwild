0159f920c83c301caf1b81fe0cb3d3529596b179
Amel252 <amelghanem25@gmail.com>
2026-07-09 14:10:53 +0200

    install react-router-dom + création des components & les routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Princing from "./pages/Pricing";
 function App() {
-    const [count, setCount] = useState(0);
-
-    return <>Woldwild</>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/pricing" element={<Princing />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
