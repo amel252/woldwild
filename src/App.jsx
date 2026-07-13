efc3b9e3d9a17805a2c4cdf5dbdbdde4a75ef519
Amel252 <amelghanem25@gmail.com>
2026-07-13 12:14:50 +0200

    Importaion de nos fichiers + organisation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Princing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 function App() {
     return (
         <BrowserRouter>
@@ -9,6 +13,9 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/product" element={<Product />} />
                 <Route path="/pricing" element={<Princing />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/app" element={<AppLayout />} />
             </Routes>
         </BrowserRouter>
     );
